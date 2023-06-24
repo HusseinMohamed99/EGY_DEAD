761c175d8399e3d3040164b33efa87dd39474238
HusseinMohamed <husseinhtm99@gmail.com>
2023-06-24 16:21:42 +0300

    Complete OnTheAir Tvs Complete AiringToday Tvs Complete Popular Tvs Complete TopRated Tvs Complete Tvs Details Complete TvsRecommendation Complete TvsSimilar
    Signed-off-by: HusseinMohamed <husseinhtm99@gmail.com>

---
+++ lib/presentation_main_app/screen/change_password_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChangePasswordScreen extends StatelessWidget {
+  const ChangePasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/presentation_main_app/screen/help_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HelpScreen extends StatelessWidget {
+  const HelpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
