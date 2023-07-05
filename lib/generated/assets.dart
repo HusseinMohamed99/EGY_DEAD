843fbd92104bffe1e4d3818caadc16e0a00440c1
HusseinMohamed <husseinhtm99@gmail.com>
2023-07-05 18:01:20 +0300

    Complete OnTheAir Tvs Complete AiringToday Tvs Complete Popular Tvs Complete TopRated Tvs Complete Tvs Details Complete TvsRecommendation Complete TvsSimilar Complete Search ThemeMode
    Signed-off-by: HusseinMohamed <husseinhtm99@gmail.com>

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -7,16 +7,20 @@ class Assets {
   static const String imagesGoogle = 'assets/images/google.svg';
   static const String imagesLinkedin = 'assets/images/linkedin.svg';
   static const String imagesLogo = 'assets/images/logo.png';
-  static const String screenShotsHomeScreen =
-      'assets/screen_shots/HomeScreen.jpg';
-  static const String screenShotsMovieDetails =
-      'assets/screen_shots/MovieDetails.jpg';
-  static const String screenShotsPopularScreen =
-      'assets/screen_shots/PopularScreen.jpg';
-  static const String screenShotsRecommendationScreen =
-      'assets/screen_shots/RecommendationScreen.jpg';
-  static const String screenShotsSimilarScreen =
-      'assets/screen_shots/SimilarScreen.jpg';
-  static const String screenShotsTopRatedScreen =
-      'assets/screen_shots/TopRatedScreen.jpg';
+  static const String screenShots1 = 'assets/screen_shots/1.png';
+  static const String screenShots10 = 'assets/screen_shots/10.jpg';
+  static const String screenShots11 = 'assets/screen_shots/11.jpg';
+  static const String screenShots12 = 'assets/screen_shots/12.jpg';
+  static const String screenShots13 = 'assets/screen_shots/13.jpg';
+  static const String screenShots14 = 'assets/screen_shots/14.jpg';
+  static const String screenShots15 = 'assets/screen_shots/15.jpg';
+  static const String screenShots16 = 'assets/screen_shots/16.jpg';
+  static const String screenShots2 = 'assets/screen_shots/2.jpg';
+  static const String screenShots3 = 'assets/screen_shots/3.jpg';
+  static const String screenShots4 = 'assets/screen_shots/4.jpg';
+  static const String screenShots5 = 'assets/screen_shots/5.jpg';
+  static const String screenShots6 = 'assets/screen_shots/6.jpg';
+  static const String screenShots7 = 'assets/screen_shots/7.jpg';
+  static const String screenShots8 = 'assets/screen_shots/8.jpg';
+  static const String screenShots9 = 'assets/screen_shots/9.jpg';
 }
