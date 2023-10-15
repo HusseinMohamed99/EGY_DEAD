017a1d442a5ede0f153d13e6fe79420a60e7fed1
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-10-15 19:32:22 +0300

    Added imports for flutter_bloc, internet_bloc, main_bloc, and main_state.
    Wrapped MaterialApp in a MultiBlocProvider with InternetCubit and MainBloc.
    
    Wrapped MaterialApp in a BlocConsumer that listens to MainBloc.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movies_app/core/global/app_string/app_string.dart';
 import 'package:movies_app/core/global/theme/theme_data/theme_data.dart';
 import 'package:movies_app/core/services/services_locator.dart';
 import 'package:movies_app/core/utils/enum.dart';
+import 'package:movies_app/presentation_main_app/controller/internet_bloc.dart';
+import 'package:movies_app/presentation_main_app/controller/main_bloc.dart';
+import 'package:movies_app/presentation_main_app/controller/main_state.dart';
 import 'package:movies_app/presentation_main_app/screen/home_page.dart';
 
 void main() async {
@@ -20,27 +24,42 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Builder(
-      builder: (context) {
-        SystemChrome.setPreferredOrientations([
-          DeviceOrientation.portraitUp,
-          DeviceOrientation.portraitDown,
-        ]);
-        return ScreenUtilInit(
-          designSize: const Size(360, 690),
-          minTextAdapt: true,
-          splitScreenMode: true,
-          builder: (context, child) {
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: AppString.appTitle,
-              theme: getThemeData[AppTheme.darkTheme],
-              darkTheme: getThemeData[AppTheme.darkTheme],
-              home: const HomePage(),
-            );
-          },
-        );
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => InternetCubit(),
+        ),
+        BlocProvider(
+          create: (context) => MainBloc(),
+        ),
+      ],
+      child: BlocConsumer<MainBloc, MainState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Builder(
+            builder: (context) {
+              SystemChrome.setPreferredOrientations([
+                DeviceOrientation.portraitUp,
+                DeviceOrientation.portraitDown,
+              ]);
+              return ScreenUtilInit(
+                designSize: const Size(360, 690),
+                minTextAdapt: true,
+                splitScreenMode: true,
+                builder: (context, child) {
+                  return MaterialApp(
+                    debugShowCheckedModeBanner: false,
+                    title: AppString.appTitle,
+                    theme: getThemeData[AppTheme.darkTheme],
+                    darkTheme: getThemeData[AppTheme.darkTheme],
+                    home: const HomePage(),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
