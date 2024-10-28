6f89f43687b90987b47b597747589f5c27862caf
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-10-28 15:48:55 +0300

    Refactor Space component constructor in size_box.dart

--- lib/core/components/size_box.dart
+++ lib/core/components/size_box.dart
@@ -4,8 +4,7 @@ class Space extends StatelessWidget {
   final double width;
   final double height;
 
-  const Space({required this.height, required this.width, Key? key})
-      : super(key: key);
+  const Space({required this.height, required this.width, super.key});
 
   @override
   Widget build(BuildContext context) {
