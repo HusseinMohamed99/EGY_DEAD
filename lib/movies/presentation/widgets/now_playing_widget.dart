ab229ff11ffcb283c97585803b9e4ddf6d449da8
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-10-29 16:37:12 +0300

    Refactor NowPlayingWidget to handle loading state with Skeletonizer
    Previously, the `NowPlayingWidget` did not handle the loading state properly, leading to a lack of visual feedback while data was being fetched. This commit refactors the widget to use the `Skeletonizer` widget conditionally based on the `RequestState`. When the state is `loaded`, the widget displays the carousel with shimmer effects. Otherwise, it shows a skeletonized version of the carousel to indicate loading. This improves the user experience by providing clear visual cues during data fetching.

--- lib/movies/presentation/widgets/now_playing_widget.dart
+++ lib/movies/presentation/widgets/now_playing_widget.dart
@@ -25,119 +25,150 @@ class NowPlayingWidget extends StatelessWidget {
       buildWhen: (previous, current) =>
           previous.nowPlayingState != current.nowPlayingState,
       builder: (context, state) {
-        return Skeletonizer(
-          enabled: false,
-          child: CarouselSlider(
-            options: CarouselOptions(
-              autoPlay: true,
-              height: 300.h,
-              viewportFraction: 1.0,
-              onPageChanged: (index, reason) {},
+        if (state.nowPlayingState == RequestState.loaded) {
+          return Skeletonizer(
+            effect: ShimmerEffect(
+              baseColor: Colors.red,
+              highlightColor: Colors.red,
+              duration: Duration(seconds: 1),
             ),
-            items: state.nowPlayingMovies.map(
-              (item) {
-                return GestureDetector(
-                  key: const Key('openMovieMinimalDetail'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) {
-                          return MovieDetailsScreen(movieID: item.id);
-                        },
-                      ),
-                    );
-                  },
-                  child: Stack(
-                    children: [
-                      ShaderMask(
-                        shaderCallback: (rect) {
-                          return const LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              Colors.transparent,
-                              Colors.black,
-                              Colors.black,
-                              Colors.transparent,
-                            ],
-                            stops: [0, 0.3, 0.5, 1],
-                          ).createShader(
-                            Rect.fromLTRB(0, 0, rect.width, rect.height),
-                          );
-                        },
-                        blendMode: BlendMode.dstIn,
-                        child: ImageWithShimmer(
-                          boxFit: BoxFit.fill,
-                          imageUrl: ApiConstance.imageURL(item.backdropPath),
-                          width: double.infinity,
-                          height: 560.h,
+            enabled: false,
+            child: CarouselSlider(
+              options: CarouselOptions(
+                autoPlay: true,
+                height: 300.h,
+                viewportFraction: 1.0,
+                onPageChanged: (index, reason) {},
+              ),
+              items: state.nowPlayingMovies.map(
+                (item) {
+                  return GestureDetector(
+                    key: const Key('openMovieMinimalDetail'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return MovieDetailsScreen(movieID: item.id);
+                          },
                         ),
-                      ),
-                      Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8.0, top: 40),
-                            child: Align(
-                              alignment: Alignment.topLeft,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(80).r,
-                                child: ImageWithShimmer(
-                                  boxFit: BoxFit.fill,
-                                  imageUrl: ApiConstance.imageURL(
-                                    item.posterPath,
+                      );
+                    },
+                    child: Stack(
+                      children: [
+                        ShaderMask(
+                          shaderCallback: (rect) {
+                            return const LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                // fromLTRB
+                                Colors.transparent,
+                                Colors.black,
+                                Colors.black,
+                                Colors.transparent,
+                              ],
+                              stops: [0, 0.3, 0.5, 1],
+                            ).createShader(
+                              Rect.fromLTRB(0, 0, rect.width, rect.height),
+                            );
+                          },
+                          blendMode: BlendMode.dstIn,
+                          child: ImageWithShimmer(
+                            boxFit: BoxFit.fill,
+                            imageUrl: ApiConstance.imageURL(item.backdropPath),
+                            width: double.infinity,
+                            height: 560.h,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 8.0, top: 40),
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(80).r,
+                                  child: ImageWithShimmer(
+                                    boxFit: BoxFit.fill,
+                                    imageUrl: ApiConstance.imageURL(
+                                      item.posterPath,
+                                    ),
+                                    width: 120.w,
+                                    height: 100.h,
                                   ),
-                                  width: 120.w,
-                                  height: 100.h,
                                 ),
                               ),
                             ),
-                          ),
-                          const Spacer(),
-                          Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.only(bottom: 16.0).r,
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Icon(
-                                        Icons.circle,
-                                        color: ColorManager.primaryRedColor,
-                                        size: 16.sp,
-                                      ),
-                                      Space(height: 0, width: 4),
-                                      Text(
-                                        AppString.nowPlaying.toUpperCase(),
-                                        style: textTheme.bodyLarge,
-                                      ),
-                                    ],
+                            const Spacer(),
+                            Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.only(bottom: 16.0).r,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.circle,
+                                          color: ColorManager.primaryRedColor,
+                                          size: 16.sp,
+                                        ),
+                                        Space(height: 0, width: 4),
+                                        Text(
+                                          AppString.nowPlaying.toUpperCase(),
+                                          style: textTheme.bodyLarge,
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(bottom: 16).r,
-                                  child: Text(
-                                    item.title,
-                                    textAlign: TextAlign.center,
-                                    style: textTheme.titleLarge,
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.only(bottom: 16).r,
+                                    child: Text(
+                                      item.title,
+                                      textAlign: TextAlign.center,
+                                      style: textTheme.titleLarge,
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ).toList(),
-          ),
-        );
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
+          );
+        } else {
+          return Skeletonizer(
+            enabled: true,
+            child: CarouselSlider(
+              options: CarouselOptions(
+                autoPlay: true,
+                height: 300,
+                viewportFraction: 1.0,
+                onPageChanged: (index, reason) {},
+              ),
+              items: [
+                ImageWithShimmer(
+                  boxFit: BoxFit.fill,
+                  imageUrl: '',
+                  width: double.infinity,
+                  height: 560,
+                ),
+              ],
+            ),
+          );
+        }
       },
     );
   }
