aa526c014f8c787aff8e53469a674836f183615c
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-10-29 14:08:36 +0300

    Refactor search module paths and implement search functionality
    - Corrected import paths in `services_locator.dart` to reflect updated file locations.
    - Implemented `SearchRemoteDataSource` and `SearchRemoteDataSourceImpl` to handle remote search requests.
    - Updated `SearchRepository` to use the correct data source path.
    - Created `SearchUseCase` to encapsulate search logic and interact with the repository.
    - Updated `SearchBloc` to use the correct use case path and handle search events.

--- lib/search/data/repository/search_repository.dart
+++ lib/search/data/repository/search_repository.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:movies_app/core/error/failure.dart';
 import 'package:movies_app/core/helpers/export_manager/export_manager.dart';
-import 'package:movies_app/search/data/datasource/search_remote_data_source.dart';
+import 'package:movies_app/search/data/data_source/search_remote_data_source.dart';
 import 'package:movies_app/search/domain/entities/search_result_item.dart';
 import 'package:movies_app/search/domain/repository/search_repository.dart';
 
