a260f28de89a78e1bd49b88245e07d261db9957e
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-11-14 12:38:35 +0200

    Enhance BuildSkeletonCarousel with detailed skeleton UI
    Add a more detailed skeleton UI for the now playing carousel, including a gradient container with an image, a "NOW PLAYING" label, and a movie title. This improves the visual feedback for users while content is loading.

--- lib/movies/presentation/widgets/build_skeleton_carousel_now_playing.dart
+++ lib/movies/presentation/widgets/build_skeleton_carousel_now_playing.dart
@@ -21,11 +21,76 @@ class BuildSkeletonCarousel extends StatelessWidget {
           onPageChanged: (index, reason) {},
         ),
         items: [
-          CachedImage(
-            boxFit: BoxFit.fill,
-            imageUrl: '',
-            width: double.infinity,
-            height: 560.h,
+          Stack(
+            children: [
+              CachedImage(
+                boxFit: BoxFit.fill,
+                imageUrl: '',
+                width: double.infinity,
+                height: 560.h,
+              ),
+              Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      margin: EdgeInsets.only(left: 8.w, top: 40.h),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 2.w, vertical: 2.h),
+                      width: 70.w,
+                      height: 60.h,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            ColorManager.whiteColor,
+                            ColorManager.primaryGreenColor
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(50).r,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(50).r,
+                        child: CachedImage(
+                          boxFit: BoxFit.fill,
+                          imageUrl: '',
+                          width: 60.w,
+                          height: 50.h,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 16.0.h),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.circle,
+                          color: ColorManager.primaryRedColor,
+                          size: 16.sp,
+                        ),
+                        Space(height: 0, width: 4),
+                        Text(
+                          AppString.nowPlaying.toUpperCase(),
+                          style: context.textTheme.bodySmall!.copyWith(
+                            color: ColorManager.whiteColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16).r,
+                    child: Text(
+                      'Transformers',
+                      textAlign: TextAlign.center,
+                      style: context.textTheme.titleLarge,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
