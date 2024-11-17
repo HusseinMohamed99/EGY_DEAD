880be84bcaabaf98977a4e75d3090220cfae331c
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-11-17 16:05:43 +0200

    Refactor AiringTodayWidget and PopularTvsWidget to use BuildContentTv and IsListLoading
    Refactor the AiringTodayWidget to use the correct error message and simplify the widget structure. Similarly, refactor the PopularTvsWidget to use BuildContentTv and IsListLoading components, and handle different states more efficiently. This change improves code readability and reusability.

--- lib/tvs/presentation/widgets/popular_tvs_widget.dart
+++ lib/tvs/presentation/widgets/popular_tvs_widget.dart
@@ -5,134 +5,32 @@ class PopularTvsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = getThemeData(context)[AppTheme.darkTheme]!.textTheme;
     return BlocBuilder<TvsBloc, TvsStates>(
       buildWhen: (previous, current) =>
           previous.popularState != current.popularState,
       builder: (context, state) {
-        switch (state.popularState) {
-          case RequestState.loading:
-            return SizedBox(
-              height: 130.h,
-              child: const LoadingIndicator(),
-            );
-          case RequestState.loaded:
-            return Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.fromLTRB(
-                    16.0,
-                    24.0,
-                    16.0,
-                    8.0,
-                  ).r,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        AppString.popular,
-                        style: textTheme.labelLarge,
-                      ),
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (BuildContext context) {
-                                return SeeMoreScreen(
-                                  tvsList: state.popularTvs,
-                                  name: AppString.popular,
-                                );
-                              },
-                            ),
-                          );
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0).r,
-                          child: Row(
-                            children: [
-                              Text(
-                                AppString.seeMore,
-                                style: textTheme.labelSmall,
-                              ),
-                              Icon(
-                                Icons.arrow_forward_ios,
-                                size: 16.sp,
-                                color: ColorManager.whiteColor,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                FadeIn(
-                  duration: const Duration(milliseconds: 500),
-                  child: SizedBox(
-                    height: 130.h,
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      padding: const EdgeInsets.symmetric(horizontal: 16).r,
-                      itemCount: state.popularTvs.length,
-                      itemBuilder: (context, index) {
-                        final tvs = state.popularTvs[index];
-                        return Container(
-                          padding: const EdgeInsets.only(right: 8).r,
-                          child: InkWell(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => TvsDetailsScreen(
-                                    tvsID: tvs.id,
-                                  ),
-                                ),
-                              );
-                              if (kDebugMode) {
-                                print(tvs.id);
-                              }
-                            },
-                            child: ClipRRect(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(8.0))
-                                      .r,
-                              child: CachedImage(
-                                imageUrl:
-                                    ApiConstance.imageURL(tvs.backdropPath),
-                                width: 120.w,
-                                height: double.infinity,
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ],
-            );
-          case RequestState.error:
-            return SizedBox(
-              height: 300.h,
-              child: Center(
-                child: Text(
-                  state.popularMessage,
-                  style: textTheme.labelLarge,
-                ),
-              ),
-            );
-          case RequestState.fetchData:
-            return SizedBox(
-              height: 300.h,
-              child: Center(
-                child: Text(
-                  'loading',
-                  style: textTheme.titleLarge,
-                ),
-              ),
-            );
+        if (state.popularState == RequestState.loading) {
+          return IsListLoading();
+        } else if (state.popularState == RequestState.loaded) {
+          state.popularTvs.shuffle(Random());
+          return BuildContentTv(
+            fetchData: false,
+            tvModel: state.popularTvs,
+            title: AppString.popular,
+            isLoading: state.popularState == RequestState.loading,
+            addEvent: () {},
+          );
+        } else if (state.popularState == RequestState.fetchData) {
+          state.popularTvs.shuffle(Random());
+          return BuildContentTv(
+            fetchData: true,
+            tvModel: state.popularTvs,
+            title: AppString.popular,
+            isLoading: state.popularState == RequestState.loading,
+            addEvent: () {},
+          );
+        } else {
+          return BuildErrorMessage(errorMessage: state.popularMessage);
         }
       },
     );
