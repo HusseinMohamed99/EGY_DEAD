f8dcf93f88cb1f284b54fa988deb560fdb5700b8
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-11-17 16:09:11 +0200

    Refactor TopRatedTvsWidget to use BuildContentTv and IsListLoading
    Simplified the TopRatedTvsWidget by replacing the switch statement with conditional checks. Utilized BuildContentTv for loaded and fetchData states and IsListLoading for the loading state. Also, added shuffling to the top-rated TV list for better user experience.

--- lib/tvs/presentation/widgets/top_rated_tvs_widget.dart
+++ lib/tvs/presentation/widgets/top_rated_tvs_widget.dart
@@ -5,134 +5,32 @@ class TopRatedTvsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = getThemeData(context)[AppTheme.darkTheme]!.textTheme;
     return BlocBuilder<TvsBloc, TvsStates>(
       buildWhen: (previous, current) =>
           previous.topRatedStates != current.topRatedStates,
       builder: (context, state) {
-        switch (state.topRatedStates) {
-          case RequestState.loading:
-            return SizedBox(
-              height: 130.h,
-              child: const LoadingIndicator(),
-            );
-          case RequestState.loaded:
-            return Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.fromLTRB(
-                    16.0,
-                    24.0,
-                    16.0,
-                    8.0,
-                  ).r,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        AppString.topRated,
-                        style: textTheme.labelLarge,
-                      ),
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (BuildContext context) {
-                                return SeeMoreScreen(
-                                  tvsList: state.topRatedTvs,
-                                  name: AppString.topRated,
-                                );
-                              },
-                            ),
-                          );
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0).r,
-                          child: Row(
-                            children: [
-                              Text(
-                                AppString.seeMore,
-                                style: textTheme.labelSmall,
-                              ),
-                              Icon(
-                                Icons.arrow_forward_ios,
-                                size: 16.sp,
-                                color: ColorManager.whiteColor,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                FadeIn(
-                  duration: const Duration(milliseconds: 500),
-                  child: SizedBox(
-                    height: 130.h,
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      padding: const EdgeInsets.symmetric(horizontal: 16).r,
-                      itemCount: state.topRatedTvs.length,
-                      itemBuilder: (context, index) {
-                        final tvs = state.topRatedTvs[index];
-                        return Container(
-                          padding: const EdgeInsets.only(right: 8.0).r,
-                          child: InkWell(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => TvsDetailsScreen(
-                                    tvsID: tvs.id,
-                                  ),
-                                ),
-                              );
-                              if (kDebugMode) {
-                                print(tvs.id);
-                              }
-                            },
-                            child: ClipRRect(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(8.0))
-                                      .r,
-                              child: CachedImage(
-                                imageUrl:
-                                    ApiConstance.imageURL(tvs.backdropPath),
-                                width: 120.w,
-                                height: double.infinity,
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ],
-            );
-          case RequestState.error:
-            return SizedBox(
-              height: 300.h,
-              child: Center(
-                child: Text(
-                  state.topRatedMessage,
-                  style: textTheme.labelLarge,
-                ),
-              ),
-            );
-          case RequestState.fetchData:
-            return SizedBox(
-              height: 300.h,
-              child: Center(
-                child: Text(
-                  'loading',
-                  style: textTheme.titleLarge,
-                ),
-              ),
-            );
+        if (state.topRatedStates == RequestState.loading) {
+          return IsListLoading();
+        } else if (state.topRatedStates == RequestState.loaded) {
+          state.topRatedTvs.shuffle(Random());
+          return BuildContentTv(
+            fetchData: false,
+            tvModel: state.topRatedTvs,
+            title: AppString.topRated,
+            isLoading: state.topRatedStates == RequestState.loading,
+            addEvent: () {},
+          );
+        } else if (state.topRatedStates == RequestState.fetchData) {
+          state.topRatedTvs.shuffle(Random());
+          return BuildContentTv(
+            fetchData: true,
+            tvModel: state.topRatedTvs,
+            title: AppString.topRated,
+            isLoading: state.topRatedStates == RequestState.loading,
+            addEvent: () {},
+          );
+        } else {
+          return BuildErrorMessage(errorMessage: state.topRatedMessage);
         }
       },
     );
